79bd943f2e5edf523decc398ad7cd8497fed2e4f
qeti252 <ketideveloper@gmail.com>
2021-03-23 15:06:01 +0400

    app pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,19 +11,40 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  // {
+  //   path: 'recipes',
+  //   children: [
+  //     {
+  //       path: '',
+  //       loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
+  //     },
+  //     {
+  //       path: ':recipeId',
+  //       loadChildren: () => import('./recipes/recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule)
+  //     }
+  //   ]
+  // },
   {
-    path: 'recipes',
-    children: [
-      {
-        path: '',
-        loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
-      },
-      {
-        path: ':recipeId',
-        loadChildren: () => import('./recipes/recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule)
-      }
-    ]
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },  {
+    path: 'search',
+    loadChildren: () => import('./places/search/search.module').then( m => m.SearchPageModule)
+  },
+  {
+    path: 'offers',
+    loadChildren: () => import('./places/offers/offers.module').then( m => m.OffersPageModule)
+  },
+  {
+    path: 'discover',
+    loadChildren: () => import('./places/discover/discover.module').then( m => m.DiscoverPageModule)
+  },
+  {
+    path: 'bookings',
+    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
   }
+
+
 ];
 
 @NgModule({
